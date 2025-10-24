de86df0fff80b3e1624c633a2285596da46034a3
icepicklife <161447128+icepicklife@users.noreply.github.com>
2025-10-24 10:07:23 +0800

    fibonacci initialize

---
+++ lab7 bsim/fibonacci_code.py
@@ -0,0 +1,26 @@
+def fibonacci_iterative(n):
+    
+
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    
+    sequence = [0, 1]  
+    
+
+    while len(sequence) < n:
+        next_num = sequence[-1] + sequence[-2]
+        sequence.append(next_num)
+        
+    return sequence
+
+
+n = int(input("enter number: "))
+
+sequence = [0, 1]
+
+while len(sequence) < n:
+    next_number = sequence[-1] + sequence[-2]
+    sequence.append(next_number)
+    
